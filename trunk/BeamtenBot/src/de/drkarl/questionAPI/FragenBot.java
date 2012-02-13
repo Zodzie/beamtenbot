3c0ae503a93cfe5cd85c924088e62f80638216f2
itgkarl <itgkarl@5fa77c8e-3f03-ad1d-a5bf-00c34c8809c8>
2012-02-13 17:12:29 +0000

    Punktesystem hinzugefügt

--- trunk/BeamtenBot/src/de/drkarl/questionAPI/FragenBot.java
+++ trunk/BeamtenBot/src/de/drkarl/questionAPI/FragenBot.java
@@ -1,15 +1,17 @@
-package de.drkarl.qestionAPI;
+package de.drkarl.questionAPI;
 
 import org.jibble.pircbot.Colors;
 import org.jibble.pircbot.PircBot;
 
-import de.drkarl.qestionAPI.fragenKatalog.Antwort;
-import de.drkarl.qestionAPI.fragenKatalog.Frage;
-import de.drkarl.qestionAPI.fragenKatalog.FragenKatalog;
+import de.drkarl.questionAPI.fragenKatalog.Antwort;
+import de.drkarl.questionAPI.fragenKatalog.Frage;
+import de.drkarl.questionAPI.fragenKatalog.FragenKatalog;
+import de.drkarl.questionAPI.punkteSystem.PunkteVerwaltung;
 
 public class FragenBot {
 
 	FragenKatalog fragenKatalog;
+	PunkteVerwaltung punkteVerwaltung;
 	Frage aktuelleFrage;
 	boolean solved = true;
 	
@@ -17,6 +19,7 @@ public class FragenBot {
 
 	public FragenBot() {
 		super();
+		punkteVerwaltung = new PunkteVerwaltung();
 		this.fragenKatalog = new FragenKatalog();
 		fragenKatalog.initParse();
 	}
@@ -46,21 +49,27 @@ public class FragenBot {
 	}
 	
 	
-	public void questionAndAnswer(String message, String channel, PircBot myBot){
+	public void questionAndAnswer(String message, String channel,String sender, PircBot myBot){
 		askQuestion(message, channel, myBot);
-		checkAnswer(message, channel, myBot);
+		checkAnswer(message, channel, sender, myBot);
 	}
 	
-	public void checkAnswer(String message, String channel, PircBot myBot){
+	public void checkAnswer(String message, String channel, String sender, PircBot myBot){
 				for (Antwort antwort : aktuelleFrage.antworten) {
 					if(message.equalsIgnoreCase(antwort.getAntwort()) || message.equalsIgnoreCase(antwort.getAntwort())){
 						//richtige Antwort
+						punkteVerwaltung.rewardUser(sender, 1);
 						myBot.sendMessage(channel, Colors.BOLD + Colors.RED + "richtige Antwort");
 						solved = true;
 					}
 				}
 	}
 	
+	public void printPunkteVerwaltung(String message, String channel, PircBot myBot){
+		if(message.equalsIgnoreCase("-stats")||message.equalsIgnoreCase("-s")){
+			myBot.sendMessage(channel, Colors.BOLD + Colors.RED + punkteVerwaltung.toString());
+		}
+	}
 	
 	public void askQuestion(String message, String channel, PircBot myBot){
 		if(message.equalsIgnoreCase("-frage")||message.equalsIgnoreCase("-f")){
@@ -71,5 +80,6 @@ public class FragenBot {
 			}
 		}
 	}
+	
 	
 }
