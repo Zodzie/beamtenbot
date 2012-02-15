1b255dd6d8a3ce2286f44593d726b30605fbdaa0
eidottermihi <eidottermihi@gmail.com>
2012-02-15 20:52:09 +0000

    TeaTimerBot hinzugefügt. TeamspeakBot baut Verbindung nach Serverabsturz selbstständig wieder auf.

---
+++ Bot/src/de/eidotter/bots/teatimerbot/TeaTimerTask.java
@@ -0,0 +1,24 @@
+package de.eidotter.bots.teatimerbot;
+
+import java.util.TimerTask;
+
+import de.hof.mainbot.Bot;
+
+public class TeaTimerTask extends TimerTask {
+	
+	private Bot mainBot;
+	private String nick;
+	private String object;
+	
+	public TeaTimerTask(Bot mainBot, String nick, String object){
+		this.mainBot = mainBot;
+		this.nick = nick;
+		this.object = object;
+	}
+
+	@Override
+	public void run() {
+		this.mainBot.sendMessage(nick, "Deine " + object + " ist fertig!");
+	}
+
+}
