8b20d5ef4f804874180cfcf2d541747f032e69d9
eidottermihi@gmail.com <eidottermihi@gmail.com@5fa77c8e-3f03-ad1d-a5bf-00c34c8809c8>
2012-03-02 14:11:01 +0000

    An NotifyBot kann nun auch eine Liste mit Nicks übergeben werden, die benachrichtigt werden sollen.

--- trunk/Bot/src/de/eidotter/bots/notifybot/NotifyBot.java
+++ trunk/Bot/src/de/eidotter/bots/notifybot/NotifyBot.java
@@ -10,8 +10,8 @@ import de.hof.mainbot.helpers.BotProperties;
 public class NotifyBot implements Observer {
 	public static final String BOT_NAME = "NotifyBot";
 	public static final String BOT_COMMAND = "-tell ";
-	public static final String BOT_HELP_SYNTAX = BOT_COMMAND + " <Nick> <Nachricht>";
-	public static final String BOT_HELP = "Schickt eine Nachricht an den angegebenen Nick, sobald dieser wieder auf den Server kommt.";
+	public static final String BOT_HELP_SYNTAX = BOT_COMMAND + " <Nick>{,<Nick>} <Nachricht>";
+	public static final String BOT_HELP = "Schickt eine Nachricht an die angegebene Nicks, sobald diese wieder auf den Server kommen.";
 	private static final String SETTINGS_FILE_NAME = "notifybot.properties";
 	private HashMap<String, ArrayList<String>> messages;
 	private BotProperties settings;
@@ -43,31 +43,34 @@ public class NotifyBot implements Observer {
 			String[] parts = message.split("\\s+");
 			if(parts.length > 2){
 				String recipient = parts[1];
+				String[] recipients = recipient.split(",");
 				String rec_message = message.substring(message.indexOf(recipient) + recipient.length() + 1);
-				this.storeMessage(recipient, rec_message, sender);
+				this.storeMessage(recipients, rec_message, sender);
 			} else {
 				// Falsches Format
 			}
 		}
 	}
 
-	private void storeMessage(String recipient, String rec_message, String sender) {
-		String toPut = recipient + ", " + sender + " lässt dir ausrichten: " + rec_message;
-		if(this.messages.containsKey(recipient.toLowerCase())){
-			// Bereits gespeicherte Nachrichten prüfen
-			if(this.messages.get(recipient.toLowerCase()).size() < this.maxMessages){
-				// Platz vorhanden
-				this.messages.get(recipient.toLowerCase()).add(toPut);
-				this.mainBot.sendMessage(sender, "Nachricht an " + recipient + " wurde gespeichert und wird ihm zugestellt, sobald dieser wieder online ist.");
+	private void storeMessage(String[] recipients, String rec_message, String sender) {
+		for (String recipient : recipients) {
+			String toPut = recipient + ", " + sender + " lässt dir ausrichten: " + rec_message;
+			if(this.messages.containsKey(recipient.toLowerCase())){
+				// Bereits gespeicherte Nachrichten prüfen
+				if(this.messages.get(recipient.toLowerCase()).size() < this.maxMessages){
+					// Platz vorhanden
+					this.messages.get(recipient.toLowerCase()).add(toPut);
+					this.mainBot.sendMessage(sender, "Nachricht an " + recipient + " wurde gespeichert und wird ihm zugestellt, sobald dieser wieder online ist.");
+				} else {
+					// Maxmimalzahl an gespeicherten Nachrichten erreicht
+					this.mainBot.sendMessage(sender, "Postfach von " + recipient + " ist leider voll!");
+				}
 			} else {
-				// Maxmimalzahl an gespeicherten Nachrichten erreicht
-				this.mainBot.sendMessage(sender, "Postfach von " + recipient + " ist leider voll!");
-			}
-		} else {
-			ArrayList<String> ar = new ArrayList<String>();
-			ar.add(toPut);
-			this.messages.put(recipient.toLowerCase(), ar);
-			this.mainBot.sendMessage(sender, "Nachricht an " + recipient + " wurde gespeichert und wird ihm zugestellt, sobald dieser wieder online ist.");
+				ArrayList<String> ar = new ArrayList<String>();
+				ar.add(toPut);
+				this.messages.put(recipient.toLowerCase(), ar);
+				this.mainBot.sendMessage(sender, "Nachricht an " + recipient + " wurde gespeichert und wird ihm zugestellt, sobald dieser wieder online ist.");
+			}			
 		}
 		
 	}
